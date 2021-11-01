8434243e27e15db1801fb61b99f06bc01d93b5c9
Francis <15894826+francisfuzz@users.noreply.github.com>
2021-11-01 15:30:27 +0000

    feat: complete ex-04 base

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -13,6 +13,9 @@ function Board() {
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
@@ -20,22 +23,27 @@ function Board() {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
+    if (winner || squares[square]) {
+      return
+    }
+
+    const nextValue = calculateNextValue(squares)
     //
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
     // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
+    const squaresCopy = [...squares]
     //
     // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
+    squaresCopy[square] = nextValue
     //
     // 🐨 set the squares to your copy
+    setSquares(squaresCopy)
   }
 
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -48,8 +56,7 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -83,6 +90,13 @@ function Game() {
 }
 
 // eslint-disable-next-line no-unused-vars
+/**
+ *
+ * @param {string|null} winner The winner of the board (if already determined)
+ * @param {array} squares
+ * @param {string} nextValue
+ * @returns {string}
+ */
 function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
@@ -92,11 +106,21 @@ function calculateStatus(winner, squares, nextValue) {
 }
 
 // eslint-disable-next-line no-unused-vars
+/**
+ * Utility function for returning the next value to be used in a tic-tac-toe
+ * @param {array} squares
+ * @returns {string} 'X' or 'O'
+ */
 function calculateNextValue(squares) {
   return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O'
 }
 
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars'
+/**
+ * Utility function for calculating the winner of a 3x3 tic-tac-toe board
+ * @param {array} squares
+ * @returns {string|null} 'X', 'O', or null
+ */
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
